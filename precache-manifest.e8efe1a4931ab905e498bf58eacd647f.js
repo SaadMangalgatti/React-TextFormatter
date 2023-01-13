036c70a79ee59565504f5a08e870b969a8f4132a
Mohammed Saad <saadmangalgatti04@gmail.com>
2023-01-13 21:17:35 +0530

    Updates

---
+++ precache-manifest.e8efe1a4931ab905e498bf58eacd647f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a84f8a08dd13a73610",
+    "url": "/SaadMangalgatti/React-TextFormatter/static/css/main.13285f7e.chunk.css"
+  },
+  {
+    "revision": "84a84f8a08dd13a73610",
+    "url": "/SaadMangalgatti/React-TextFormatter/static/js/main.84a84f8a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/SaadMangalgatti/React-TextFormatter/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/SaadMangalgatti/React-TextFormatter/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "f9656d16154c794848d5",
+    "url": "/SaadMangalgatti/React-TextFormatter/static/js/runtime~main.f9656d16.js"
+  },
+  {
+    "revision": "07a51fd8704a231b545d836efc070020",
+    "url": "/SaadMangalgatti/React-TextFormatter/index.html"
+  }
+];
